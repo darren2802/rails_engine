84f9b24626853e10bfd0bfd8944fde5cc1e9e062
Darren Campbell <darren2802@gmail.com>
2020-01-28 16:58:59 -0700

    Adds functionality for relationship endpoint merchant items and tests thereof

--- rails_engine_api/config/routes.rb
+++ rails_engine_api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index]
+      get '/merchants/:id/items', to: 'merchant_items#index'
       resources :items, only: [:index]
       resources :customers, only: [:index]
       resources :invoices, only: [:index]
