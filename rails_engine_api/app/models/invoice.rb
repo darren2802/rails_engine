5bde20475c2caa1b74882a0f6203c47d56d82370
Darren Campbell <darren2802@gmail.com>
2020-01-28 14:31:16 -0700

    Removes validation of timestamps

--- rails_engine_api/app/models/invoice.rb
+++ rails_engine_api/app/models/invoice.rb
@@ -1,5 +1,5 @@
 class Invoice < ApplicationRecord
-  validates_presence_of :status, :created_at, :updated_at
+  validates_presence_of :status
 
   has_many :invoice_items
   has_many :items, through: :invoice_items
--- rails_engine_api/app/models/invoice_item.rb
+++ rails_engine_api/app/models/invoice_item.rb
@@ -1,5 +1,5 @@
 class InvoiceItem < ApplicationRecord
-  validates_presence_of :quantity, :unit_price, :created_at, :updated_at
+  validates_presence_of :quantity, :unit_price
 
   belongs_to :item
   belongs_to :invoice
