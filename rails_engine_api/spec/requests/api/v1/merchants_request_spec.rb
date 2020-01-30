a3a4e07d1c21a2606fcbf32ae3fed26d0c926fd5
Darren Campbell <darren2802@gmail.com>
2020-01-29 20:34:02 -0700

    Moves relationship tests to main request test file for merchants

--- rails_engine_api/spec/requests/api/v1/merchants_request_spec.rb
+++ rails_engine_api/spec/requests/api/v1/merchants_request_spec.rb
@@ -23,6 +23,31 @@ describe 'Merchants API' do
     expect(merchant['data']['attributes']['name']).to eq(merchant_2.name)
   end
 
+  it 'can send a list of items for a particular merchant' do
+    merchant_id_1 = create(:merchant).id
+    create_list(:item, 4, merchant_id: merchant_id_1)
+    merchant_id_2 = create(:merchant).id
+    create_list(:item, 2, merchant_id: merchant_id_2)
+
+    get "/api/v1/merchants/#{merchant_id_2}/items"
+    expect(response).to be_successful
+    merchant_items = JSON.parse(response.body)
+    expect(merchant_items['data'].count).to eq(2)
+  end
+
+  it 'can send a list of invoices for a particular merchant' do
+    customer_id = create(:customer).id
+    merchant_id_1 = create(:merchant).id
+    create_list(:invoice, 3, customer_id: customer_id, merchant_id: merchant_id_1)
+    merchant_id_2 = create(:merchant).id
+    create_list(:invoice, 5, customer_id: customer_id, merchant_id: merchant_id_2)
+
+    get "/api/v1/merchants/#{merchant_id_2}/invoices"
+    expect(response).to be_successful
+    merchant_invoices = JSON.parse(response.body)
+    expect(merchant_invoices['data'].count).to eq(5)
+  end
+
   it 'returns the top x merchants ranked by total revenue' do
     customer_id_1 = create(:customer).id
     merchant_id_1 = create(:merchant).id
